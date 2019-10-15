dd4510540d3a5c4af6a6d46de15fa4c3b127f048
Janik Mayr <jan.222.ik@gmail.com>
2019-10-15 20:23:53 +0200

    Added main java and kotlin class for compiler checks and cross-language calls. Removed Placeholder file.

---
+++ persistence/src/main/java/at.fhv.itb17.s5.teamb.persistance/Main.kt
@@ -0,0 +1,7 @@
+package at.fhv.itb17.s5.teamb.persistance
+
+class Main {
+    fun main(args: List<String>) {
+        println("Hello World! (Passed List: $args)")
+    }
+}
---
+++ persistence/src/main/java/at.fhv.itb17.s5.teamb.persistance/Main2.java
@@ -0,0 +1,13 @@
+package at.fhv.itb17.s5.teamb.persistance;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main2 {
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<>();
+        list.add("Element 0");
+        list.add("Element 1");
+        new Main().main(list);
+    }
+}
