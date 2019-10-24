9ac94f56220da3724e8cee3610079f0031356fd7
Janik Mayr <jan.222.ik@gmail.com>
2019-10-24 22:24:45 +0200

    Added Reset action for filter screen

--- client.javafx/src/main/java/at/fhv/itb17/s5/teamb/fxapp/views/content/search/SearchPresenter.java
+++ client.javafx/src/main/java/at/fhv/itb17/s5/teamb/fxapp/views/content/search/SearchPresenter.java
@@ -5,6 +5,10 @@ import at.fhv.itb17.s5.teamb.fxapp.viewmodel.ContentfulViewLifeCycle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.GridPane;
@@ -13,6 +17,7 @@ import javafx.scene.paint.Paint;
 
 import javax.inject.Inject;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 public class SearchPresenter implements ContentfulViewLifeCycle, Initializable {
@@ -31,6 +36,19 @@ public class SearchPresenter implements ContentfulViewLifeCycle, Initializable {
     @FXML
     private Button searchBtn;
 
+    @FXML
+    private CheckBox fromCB;
+    public DatePicker fromDateDP;
+    @FXML
+    private CheckBox tillCB;
+    public DatePicker tillDateDP;
+    @FXML
+    private TextField eventTE;
+    @FXML
+    private TextField artistTE;
+    @FXML
+    private ChoiceBox genreChoiceBox;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Background defaultBack = style.SURFACE().asBackground();
@@ -42,5 +60,17 @@ public class SearchPresenter implements ContentfulViewLifeCycle, Initializable {
         actionEmitter.setBackground(defaultBack);
         style.hoverBtn(searchBtn, defaultBack, onDefaultPaint, defaultBack, style.PRIMARY().asPaint());
         style.hoverBtn(resetBtn, defaultBack, onDefaultPaint, defaultBack, style.ERROR().asPaint());
+        resetBtn.setOnAction(e -> resetFilter());
+        resetFilter();
+    }
+
+    private void resetFilter() {
+        eventTE.clear();
+        fromCB.setSelected(false);
+        fromDateDP.setValue(LocalDate.now());
+        tillCB.setSelected(false);
+        tillDateDP.setValue(LocalDate.now().plusMonths(1L));
+        genreChoiceBox.getSelectionModel().selectFirst();
+        artistTE.clear();
     }
 }
