77230b6fa217fda2ad6505915a7481b416765a0d
Janik Mayr <jan.222.ik@gmail.com>
2019-10-28 21:00:02 +0100

    Added data package with Service and temporary DTO. Added new ViewModel classes.

---
+++ client.javafx/src/main/java/at/fhv/itb17/s5/teamb/fxapp/data/PlaceholderDTO.java
@@ -0,0 +1,5 @@
+package at.fhv.itb17.s5.teamb.fxapp.data;
+
+@Deprecated
+public class PlaceholderDTO {
+}
--- client.javafx/src/main/java/at/fhv/itb17/s5/teamb/fxapp/viewmodel/SearchVM.java
+++ client.javafx/src/main/java/at/fhv/itb17/s5/teamb/fxapp/viewmodel/SearchVM.java
@@ -1,23 +1,23 @@
 package at.fhv.itb17.s5.teamb.fxapp.viewmodel;
 
+import at.fhv.itb17.s5.teamb.core.SearchParser;
+
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SearchVM implements ViewModel {
-    private SearchViewState latestSearchViewState = null;
+    private SearchViewData latestSearchViewState = null;
 
-    public SearchVM() {
-
-    }
-
-    public SearchViewState getLatestSearchViewState() {
+    public SearchViewData getLatestSearchViewState() {
         return latestSearchViewState;
     }
 
-    public void setLatestSearchViewState(SearchViewState latestSearchViewState) {
-        this.latestSearchViewState = latestSearchViewState;
+    public void setLatestSearchViewState(SearchViewData latestSearchViewData) {
+        this.latestSearchViewState = latestSearchViewData;
     }
 
-    public static class SearchViewState {
+    public static class SearchViewData {
+        private static final DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         public boolean includeFrom;
         public LocalDate fromDate;
         public boolean includeTill;
@@ -25,5 +25,26 @@ public class SearchVM implements ViewModel {
         public String event;
         public String artist;
         public int genre;
+        public String genreValue;
+
+        public String toQueryString() {
+            StringBuilder sb = new StringBuilder();
+            if (includeFrom) {
+                sb.append("-").append(SearchParser.SearchCategories.DATE_FROM).append(fromDate.format(format));
+            }
+            if (includeTill) {
+                sb.append("-").append(SearchParser.SearchCategories.DATE_UNTIL).append(tillDate.format(format));
+            }
+            if (!event.isEmpty()) {
+                sb.append("-").append(SearchParser.SearchCategories.EVENT_NAME).append(event);
+            }
+            if (!artist.isEmpty()) {
+                sb.append("-").append(SearchParser.SearchCategories.ARTIST_NAME).append(artist);
+            }
+            if (!genreValue.isEmpty()) {
+                sb.append("-").append(SearchParser.SearchCategories.GENRE).append(genreValue);
+            }
+            return sb.toString();
+        }
     }
 }
