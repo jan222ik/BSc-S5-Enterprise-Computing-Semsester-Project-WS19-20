cfdbe8fe52998b1b938f886ded246f16a60847aa
Janik Mayr <jan.222.ik@gmail.com>
2019-11-18 18:03:42 +0100

    LDAP Tests

--- util/src/main/java/at/fhv/itb17/s5/teamb/util/LDAP.java
+++ util/src/main/java/at/fhv/itb17/s5/teamb/util/LDAP.java
@@ -15,7 +15,7 @@ import java.util.Hashtable;
 
 public class LDAP {
 
-    private DirContext getLDAPConnection(String commonName, String organisationUnit, String password) throws NamingException {
+    DirContext getLDAPConnection(String commonName, String organisationUnit, String password) throws NamingException {
         Hashtable<String, String> env = new Hashtable<>();
         env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
         env.put(Context.PROVIDER_URL, "ldaps://dc01.ad.uclv.net:636/OU=fhusers,DC=ad,DC=uclv,DC=net");
@@ -23,13 +23,6 @@ public class LDAP {
         env.put(Context.SECURITY_PRINCIPAL, String.format("CN=%s,OU=%s,DC=ad,DC=uclv,DC=net", commonName, organisationUnit));
         env.put(Context.SECURITY_CREDENTIALS, password);
         return new InitialDirContext(env);
-    }
-
-    //arg[0] = mle2266@students.fhv.at - or other similar username
-    //arg[1] = password
-    public static void main(String... args) throws NamingException {
-        LDAP ldap = new LDAP();
-        System.out.println(ldap.areCredentialsCorrect(args[0], args[1]));
     }
 
     public boolean areCredentialsCorrect(String username, String password) throws NamingException {
