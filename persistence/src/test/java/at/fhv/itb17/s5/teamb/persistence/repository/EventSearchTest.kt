897580ba86af7d5add6e2b5022ba426dd14c670f
Janik Mayr <jan.222.ik@gmail.com>
2019-11-18 18:41:34 +0100

    Tests Layout for Search

---
+++ persistence/src/test/java/at/fhv/itb17/s5/teamb/persistence/repository/EventSearchTest.kt
@@ -0,0 +1,71 @@
+package at.fhv.itb17.s5.teamb.persistence.repository
+
+import org.junit.jupiter.api.Test
+
+class EventSearchTest{
+    private var entityRepository: EntityRepository = EntityRepository()
+    private var eventRepository: EventRepository = EventRepository(entityRepository)
+
+    init {
+        TODO("Insert data into database")
+    }
+
+    @Test
+    fun `Search Tickets - Success - Find All`() {
+        TODO("Implement Test")
+    }
+
+    @Test
+    fun `Search Tickets - Success - by event title`() {
+        TODO("Implement Test")
+    }
+
+    @Test
+    fun `Search Tickets - Fail - by event title`() {
+        TODO("Implement Test")
+    }
+
+    @Test
+    fun `Search Tickets - Success - by event genre`() {
+        TODO("Implement Test")
+    }
+
+    @Test
+    fun `Search Tickets - Fail - by event genre`() {
+        TODO("Implement Test")
+    }
+
+    @Test
+    fun `Search Tickets - Success - by occurrence from date`() {
+        TODO("Implement Test")
+    }
+
+    @Test
+    fun `Search Tickets - Fail - by occurrence from date`() {
+        TODO("Implement Test")
+    }
+
+    @Test
+    fun `Search Tickets - Success - by occurrence until date`() {
+        TODO("Implement Test")
+    }
+
+    @Test
+    fun `Search Tickets - Fail - by occurrence until date`() {
+        TODO("Implement Test")
+    }
+
+    @Test
+    fun `Search Tickets - Success - by location`() {
+        TODO("Implement Test")
+    }
+
+    @Test
+    fun `Search Tickets - Fail - by location`() {
+        TODO("Implement Test")
+    }
+
+
+
+
+}
