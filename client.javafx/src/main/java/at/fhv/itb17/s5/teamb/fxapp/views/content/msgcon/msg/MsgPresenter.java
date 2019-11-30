986f8e8e9792a08e2031a31b259c1948890624c7
Janik Mayr <jan.222.ik@gmail.com>
2019-11-30 16:56:30 +0100

    Styling update for msgcon

--- client.javafx/src/main/java/at/fhv/itb17/s5/teamb/fxapp/views/content/msgcon/msg/MsgPresenter.java
+++ client.javafx/src/main/java/at/fhv/itb17/s5/teamb/fxapp/views/content/msgcon/msg/MsgPresenter.java
@@ -1,12 +1,18 @@
 package at.fhv.itb17.s5.teamb.fxapp.views.content.msgcon.msg;
 
 import at.fhv.itb17.s5.teamb.fxapp.data.MsgWrapper;
+import at.fhv.itb17.s5.teamb.fxapp.style.Style;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import org.jetbrains.annotations.NotNull;
 
+import javax.inject.Inject;
+
 public class MsgPresenter {
+
+    @Inject
+    private Style style;
 
     @FXML
     private Label topicL;
@@ -18,6 +24,7 @@ public class MsgPresenter {
     private Button ackBtn;
 
     public void displayMsg(@NotNull MsgWrapper msg, @NotNull Runnable onAck) {
+        style.hoverBtn(ackBtn, style.SURFACE().asBackground(), style.ON_SURFACE().asPaint(), style.SURFACE().asBackground(), style.PRIMARY().asPaint());
         ackBtn.setOnAction(e -> onAck.run());
         topicL.setText(msg.getTopicName());
         headerL.setText(msg.getHeader());
