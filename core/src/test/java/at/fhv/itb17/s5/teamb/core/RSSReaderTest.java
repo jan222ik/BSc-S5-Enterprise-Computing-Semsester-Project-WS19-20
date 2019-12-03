35cc986247794bd1314a250d0420b48268aeb554
Janik Mayr <jan.222.ik@gmail.com>
2019-12-03 20:01:45 +0100

    Added Test class for Rss reader

---
+++ core/src/test/java/at/fhv/itb17/s5/teamb/core/RSSReaderTest.java
@@ -0,0 +1,13 @@
+package at.fhv.itb17.s5.teamb.core;
+
+import org.junit.jupiter.api.Test;
+
+public class RSSReaderTest {
+
+    public static final String URL_FEED_1 = "https://www.ots.at/rss/kultur";
+
+    @Test
+    public void test0() {
+
+    }
+}
