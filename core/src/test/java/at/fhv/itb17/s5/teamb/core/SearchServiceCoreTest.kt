960ab81f8478ee4953bf997fc511bed7f746fc14
Janik Mayr <jan.222.ik@gmail.com>
2019-12-06 21:40:40 +0100

    Fixed search error and implemented test on core level

---
+++ core/src/test/java/at/fhv/itb17/s5/teamb/core/SearchServiceCoreTest.kt
@@ -0,0 +1,127 @@
+package at.fhv.itb17.s5.teamb.core
+
+import at.fhv.itb17.s5.teamb.core.domain.general.CoreServiceInjectorImpl
+import at.fhv.itb17.s5.teamb.persistence.entities.Event
+import at.fhv.itb17.s5.teamb.persistence.search.SearchCategories
+import at.fhv.itb17.s5.teamb.persistence.search.SearchPair
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.stream.Collectors
+
+class SearchServiceCoreTest {
+
+    private val now: String = LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+    private val in7: String = LocalDate.now().plusDays(7L).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+    private val evtName: String = "A fancy concert"
+    private val validString: String = "-until=\"$now\" -from=\"$in7\" -title=\"$evtName\""
+    private val validSingle: String = "-title=\"$evtName\""
+
+    private fun toSearchString(cats: List<SearchPair>): String {
+        return cats.stream().map { e -> "-" + e.key.getIdf() + "=\"" + e.value.substringBefore('-') + "\"" }.collect(Collectors.joining(" "))
+    }
+
+    @Test
+    fun `Search Tickets - Success - Find All`() {
+        val coreServiceInjectorImpl = CoreServiceInjectorImpl(false)
+        val search = coreServiceInjectorImpl.searchServiceCore.searchFor("")
+        assertThat(search.size, `is`(5))
+    }
+
+    @Test
+    fun `Search Tickets - Success - by event title`() {
+        val coreServiceInjectorImpl = CoreServiceInjectorImpl(false)
+        val evt = coreServiceInjectorImpl.entityRepository.getAll(Event::class.java, listOf())[0]
+        val pair = toSearchString(listOf(SearchPair(SearchCategories.EVENT_NAME, evt.title)))
+        val search = coreServiceInjectorImpl.searchServiceCore.searchFor(pair)
+        assertThat(search.size, Matchers.`is`(2))
+        val anyMatch = search.stream().anyMatch { it.eventId == evt.eventId }
+        assertTrue(anyMatch)
+    }
+
+    @Test
+    fun `Search Tickets - Success - by artist name`() {
+        val coreServiceInjectorImpl = CoreServiceInjectorImpl(false)
+        val evt = coreServiceInjectorImpl.entityRepository.getAll(Event::class.java, listOf())[0]
+        val pair = toSearchString(listOf(SearchPair(SearchCategories.ARTIST_NAME, evt.artists[0].name)))
+        val search = coreServiceInjectorImpl.searchServiceCore.searchFor(pair)
+        assertThat(search.size, Matchers.`is`(1))
+        val anyMatch = search.stream().anyMatch { it.eventId == evt.eventId }
+        assertTrue(anyMatch)
+    }
+
+    @Test
+    fun `Search Tickets - Fail - by event title`() {
+        val coreServiceInjectorImpl = CoreServiceInjectorImpl(false)
+        val pair = toSearchString(listOf(SearchPair(SearchCategories.EVENT_NAME, "DOESNOTEXIST")))
+        println("pair = ${pair}")
+        val search = coreServiceInjectorImpl.searchServiceCore.searchFor(pair)
+        assertThat(search.size, Matchers.`is`(0))
+    }
+
+    @Test
+    fun `Search Tickets - Success - by event genre`() {
+        val coreServiceInjectorImpl = CoreServiceInjectorImpl(false)
+        val evt = coreServiceInjectorImpl.entityRepository.getAll(Event::class.java, listOf())[4]
+        val pair = toSearchString(listOf(SearchPair(SearchCategories.GENRE, evt.genre)))
+        val search = coreServiceInjectorImpl.searchServiceCore.searchFor(pair)
+        assertThat(search.size, Matchers.`is`(2))
+        val anyMatch = search.stream().allMatch {
+            it.genre == evt.genre
+        }
+        assertTrue(anyMatch)
+    }
+
+    @Test
+    fun `Search Tickets - Fail - by event genre`() {
+        val coreServiceInjectorImpl = CoreServiceInjectorImpl(false)
+        val pair = toSearchString(listOf(SearchPair(SearchCategories.GENRE, "DOESNOTEXIST")))
+        val search = coreServiceInjectorImpl.searchServiceCore.searchFor(pair)
+        assertThat(search.size, Matchers.`is`(0))
+    }
+
+    @Test
+    fun `Search Tickets - Success - by date from and genre`() {
+        val coreServiceInjectorImpl = CoreServiceInjectorImpl(false)
+        val evt = coreServiceInjectorImpl.entityRepository.getAll(Event::class.java, listOf())[4]
+        val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+        val pair = toSearchString(listOf(SearchPair(SearchCategories.GENRE, evt.genre), SearchPair(SearchCategories.DATE_FROM, evt.occurrences.get(0).date.format(formatter))))
+        val search = coreServiceInjectorImpl.searchServiceCore.searchFor(pair)
+        assertThat(search.size, Matchers.`is`(1))
+        val anyMatch = search.stream().allMatch {
+            it.genre == evt.genre
+        }
+        assertTrue(anyMatch)
+    }
+
+    @Test
+    fun `Search Tickets - Success - by date until and genre`() {
+        val coreServiceInjectorImpl = CoreServiceInjectorImpl(false)
+        val evt = coreServiceInjectorImpl.entityRepository.getAll(Event::class.java, listOf())[4]
+        val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+        val pair = toSearchString(listOf(SearchPair(SearchCategories.GENRE, evt.genre), SearchPair(SearchCategories.DATE_UNTIL, evt.occurrences.get(0).date.plusDays(1L).format(formatter))))
+        val search = coreServiceInjectorImpl.searchServiceCore.searchFor(pair)
+        assertThat(search.size, Matchers.`is`(1))
+        val anyMatch = search.stream().allMatch {
+            it.genre == evt.genre
+        }
+        assertTrue(anyMatch)
+    }
+
+    @Test
+    fun `Search Tickets - Success - by location`() {
+        val coreServiceInjectorImpl = CoreServiceInjectorImpl(false)
+        val evt = coreServiceInjectorImpl.entityRepository.getAll(Event::class.java, listOf())[4]
+        val pair = toSearchString(listOf(SearchPair(SearchCategories.LOCATION, evt.occurrences[0].address.city)))
+        val search = coreServiceInjectorImpl.searchServiceCore.searchFor(pair)
+        assertThat(search.size, Matchers.`is`(2))
+        val anyMatch = search.stream().allMatch {
+            it.occurrences[0].address.city == evt.occurrences[0].address.city
+        }
+        assertTrue(anyMatch)
+    }
+}
--- persistence/src/test/java/at/fhv/itb17/s5/teamb/persistence/repository/EventSearchTest.kt
+++ persistence/src/test/java/at/fhv/itb17/s5/teamb/persistence/repository/EventSearchTest.kt
@@ -2,7 +2,11 @@ package at.fhv.itb17.s5.teamb.persistence.repository
 
 import at.fhv.itb17.s5.teamb.persistence.entities.Event
 import at.fhv.itb17.s5.teamb.persistence.provider.EventProvider
-import org.junit.jupiter.api.BeforeEach
+import at.fhv.itb17.s5.teamb.persistence.search.SearchCategories
+import at.fhv.itb17.s5.teamb.persistence.search.SearchPair
+import junit.framework.Assert.assertTrue
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers
 import org.junit.jupiter.api.Test
 import java.util.*
 
@@ -10,38 +14,21 @@ class EventSearchTest {
 
     private var entityRepository: EntityRepository = EntityRepository()
     private var eventRepository: EventRepository = EventRepository(entityRepository)
-    private var e1: Event = EventProvider.getNewEventAndAddDB(entityRepository, "e1")
-    private var e2: Event = EventProvider.getNewEventAndAddDB(entityRepository, "e2")
-    private var e3: Event = EventProvider.getNewEventAndAddDB(entityRepository, "e3")
-    private var e4: Event = EventProvider.getNewEventAndAddDB(entityRepository, "e4")
-    private var e5: Event = EventProvider.getNewEventAndAddDB(entityRepository, "e5")
+    private var e1: Event = EventProvider.getNewEventAndAddDB("e1")
+    private var e2: Event = EventProvider.getNewEventAndAddDB("e2")
+    private var e3: Event = EventProvider.getNewEventAndAddDB("e3")
+    private var e4: Event = EventProvider.getNewEventAndAddDB("e4")
+    private var e5: Event = EventProvider.getNewEventAndAddDB("e5")
     private var INVALID: Event = EventProvider.getNewTransientEvent("invalid")
     private var allEvents: List<Event> = LinkedList(listOf(e1, e2, e3, e4, e5))
 
-    @BeforeEach
-    internal fun setUp() {
-        println("Before each")
-        e1 = EventProvider.getNewEventAndAddDB(entityRepository, "e1")
-        e2 = EventProvider.getNewEventAndAddDB(entityRepository, "e2")
-        e3 = EventProvider.getNewEventAndAddDB(entityRepository, "e3")
-        e4 = EventProvider.getNewEventAndAddDB(entityRepository, "e4")
-        e5 = EventProvider.getNewEventAndAddDB(entityRepository, "e5")
+    fun upsert(evts: List<Event>) {
+        evts.forEach { e -> entityRepository.saveOrUpdate(e) }
     }
-
-    @Test
-    fun a() {
-
-    }
-
-    @Test
-    fun a2() {
-
-    }
-
-    /* FIXME
 
     @Test
     fun `Search Tickets - Success - Find All`() {
+        upsert(allEvents)
         val search = eventRepository.search(listOf())
         search.forEach {
             assertTrue(e1.eventId == it.eventId || e2.eventId == it.eventId || e3.eventId == it.eventId || e4.eventId == it.eventId || e5.eventId == it.eventId)
@@ -50,6 +37,7 @@ class EventSearchTest {
 
     @Test
     fun `Search Tickets - Success - by event title`() {
+        upsert(allEvents)
         val search = eventRepository.search(listOf(SearchPair(SearchCategories.EVENT_NAME, e1.title)))
         assertThat(search.size, Matchers.`is`(1))
         search.forEach {
@@ -59,12 +47,14 @@ class EventSearchTest {
 
     @Test
     fun `Search Tickets - Fail - by event title`() {
+        upsert(allEvents)
         val search = eventRepository.search(listOf(SearchPair(SearchCategories.EVENT_NAME, INVALID.title)))
         assertThat(search.size, Matchers.`is`(0))
     }
 
     @Test
     fun `Search Tickets - Success - by event genre`() {
+        upsert(allEvents)
         val search = eventRepository.search(listOf(SearchPair(SearchCategories.GENRE, e1.genre)))
         assertThat(search.size, Matchers.`is`(1))
         search.forEach {
@@ -74,11 +64,11 @@ class EventSearchTest {
 
     @Test
     fun `Search Tickets - Fail - by event genre`() {
+        upsert(allEvents)
         val search = eventRepository.search(listOf(SearchPair(SearchCategories.EVENT_NAME, INVALID.genre)))
         assertThat(search.size, Matchers.`is`(0))
     }
 
-     */
 
     /*
     @Test
