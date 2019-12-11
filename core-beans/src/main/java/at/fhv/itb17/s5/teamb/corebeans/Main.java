439c22a0d879f4c4e6aea3f6bcfadad37124651f
Janik Mayr <jan.222.ik@gmail.com>
2019-12-12 00:15:51 +0100

    WIP: Repair JAR with Dependencies build

--- core-beans/src/main/java/at/fhv/itb17/s5/teamb/corebeans/Main.java
+++ core-beans/src/main/java/at/fhv/itb17/s5/teamb/corebeans/Main.java
@@ -1,6 +1,7 @@
 package at.fhv.itb17.s5.teamb.corebeans;
 
-import at.fhv.itb17.s5.teamb.core.domain.general.CoreMain;
+
+import at.fhv.itb17.s5.teamb.corestarter.CoreMain;
 
 public class Main {
     public static void main(String[] args) {
