d365d700a12473f36dc5ca49af77aaa9cb79dcb1
Janik Mayr <jan.222.ik@gmail.com>
2019-12-19 03:11:31 +0100

    Update

--- webjs/src/Handler.kt
+++ webjs/src/Handler.kt
@@ -1,41 +1,55 @@
-import org.w3c.dom.Element
-import org.w3c.dom.HTMLButtonElement
-import org.w3c.dom.HTMLElement
+import org.w3c.dom.*
 import kotlin.browser.document
 import kotlin.browser.window
-import kotlin.dom.clear
-import kotlin.dom.createElement
+import kotlin.dom.*
 import kotlin.js.Promise
 
-class Handler {
-    val testURL: String = "http://localhost:8080"
-    val searchURL: String = testURL + "/events/findByQueryString?queryString="
+object Handler {
+    private const val testURL: String = "http://localhost:8080"
+    private const val searchURL: String = "$testURL/events/findByQueryString?queryString="
+    private var spinners: HashMap<HTMLInputElement, PriceCategory> = hashMapOf()
+    var latestSearchQuery: String = ""
 
 
-    fun getSearchResults(searchQuery: String): Promise<List<EventDTO>> {
-        return window.fetch(searchURL + searchQuery).then { res ->
-            return@then res.text()
+    private fun getSearchResults(searchQuery: String): Promise<List<EventDTO>> {
+        return window.fetch(input = searchURL + searchQuery).then outerThen@{ res ->
+            val text = res.text()
+            return@outerThen Promise<Pair<String, Short>> { resolve, reject ->
+                text.then { resolve(Pair(it, res.status)) }.catch(reject)
+            }
         }.then {
-            return@then JSON.parse<Array<EventDTO>>(it).toList()
+            return@then when (it.second) {
+                200.toShort() -> JSON.parse<Array<EventDTO>>(it.first).toList()
+                204.toShort() -> emptyList()
+                else -> {
+                    OnPageAlert.showErr("Server exception occurred: ${it.second}")
+                    emptyList()
+                }
+            }
         }.catch { trow ->
             console.log("Caught: $trow")
+            OnPageAlert.showErr("Server exception occurred")
             return@catch emptyList<EventDTO>()
         }
     }
-}
 
-object DomModifier {
-    fun populateContainer(eventDTOs: List<EventDTO>) {
+    private fun populateContainer(eventDTOs: List<EventDTO>) {
         val container = document.getElementById("container") as HTMLElement
         container.clear()
-        for (eventDTO in eventDTOs) {
-            container.append(newEventDiv(eventDTO = eventDTO), document.createElement("hr"))
+        if (eventDTOs.isNotEmpty()) {
+            for (eventDTO in eventDTOs) {
+                container.append(newEventDiv(eventDTO = eventDTO), document.createElement("hr"))
+            }
+            OnPageAlert.showInfo("Found ${eventDTOs.size} events")
+        } else {
+            container.appendElement("h3") { textContent = "No results found" }
+            OnPageAlert.showInfo("No events found")
         }
     }
 
     private fun newEventDiv(eventDTO: EventDTO): Element {
         return document.createElement("div") {
-            this.className = "event-element-conatiner"
+            this.className = "event-element-container"
             val titleL = label(text = "Title", cssExt = "title")
             val title = span(text = eventDTO.title, cssExt = "title")
             val genreL = label(text = "Genre", cssExt = "genre")
@@ -43,14 +57,13 @@ object DomModifier {
             val occL = label(text = "Occurrences", cssExt = "occurrences")
             val occContainer = document.createElement("div") {
                 this.className = "occ-container"
-                val table = document.createElement("table") {
+                this.appendElement("table") {
                     this.className = "occ-table"
-
                     val header = document.createElement("thead") {
                         this.append(occHeader())
                     }
-                    val occurrences = eventDTO.occurrences
                     val body = document.createElement("tbody")
+                    val occurrences = eventDTO.occurrences
                     for (i in occurrences.indices) {
                         console.dir(occurrences[i])
                         body.appendChild(occRow(eventDTO, occurrences[i]))
@@ -58,7 +71,6 @@ object DomModifier {
                     this.appendChild(header)
                     this.appendChild(body)
                 }
-                this.append(table)
             }
             this.append(titleL, title, br(), genreL, genre, br(), occL, br(), occContainer)
         }
@@ -103,6 +115,100 @@ object DomModifier {
 
     private fun showDetail(event: EventDTO, occurrence: Occurrence) {
         console.log("Detail for ${event.eventId} - Occ: ${occurrence.occurrenceId}")
+        val container = document.getElementById("container")!!
+        container.clear()
+        container.append(detailView(event, occurrence))
+        updateTotal()
+    }
+
+    private fun detailView(event: EventDTO, occurrence: Occurrence): Element {
+        return document.createElement("div") {
+            className = "detail-div-root"
+            append(detailViewInfo(event, occurrence))
+            val pair = detailViewCategories(occurrence)
+            append(pair.first)
+            spinners = pair.second as HashMap<HTMLInputElement, PriceCategory>
+            appendElement("div") { id = "detail-total-view" }
+            append(detailViewActions(event, occurrence))
+        }
+    }
+
+    private fun detailViewActions(event: EventDTO, occurrence: Occurrence): Element {
+        return document.createElement("div") {
+            appendElement("button") {
+                val btn = this as HTMLButtonElement
+                btn.textContent = "Buy"
+                btn.addEventListener(type = "click", callback = {
+                    val count = addCurrentToCart(event, occurrence)
+                    if (count > 0) {
+                        OnPageAlert.showInfo("Added $count items to cart.")
+                        openCart()
+                    }
+                })
+            }
+        }
+    }
+
+    private fun addCurrentToCart(event: EventDTO, occurrence: Occurrence): Int {
+        var count = 0
+        for (spinner in spinners) {
+            val amount = spinner.key.value.toInt()
+            for (i in 0 until amount) {
+                Cart.add(LocalTicket(event, occurrence, spinner.value))
+                count++
+            }
+        }
+        return count
+    }
+
+    private fun detailViewCategories(occurrence: Occurrence): Pair<Element, MutableMap<HTMLInputElement, PriceCategory>> {
+        val map = mutableMapOf<HTMLInputElement, PriceCategory>()
+        val createElement = document.createElement("div") {
+            occurrence.priceCategories.forEach {
+                appendElement("div") {
+                    val spinner = document.createElement("input") as HTMLInputElement
+                    spinner.type = "number"
+                    spinner.min = "0"
+                    spinner.max = "${it.totalTickets - it.usedTickets}"
+                    spinner.step = "1"
+                    spinner.value = "0"
+                    spinner.onkeypress = { _ -> updateTotal() }
+                    map[spinner] = it
+                    append(
+                            span(it.categoryName, "cat-name"),
+                            span(it.toDisplayPrice(), "cat-price"),
+                            span(it.ticketRange(), "cat-ticket-range"),
+                            spinner
+                    )
+                }
+            }
+        }
+        return Pair(createElement, map)
+    }
+
+    private fun updateTotal() {
+        var totalPrice: Int = 0
+        var ticketAmount: Int = 0
+        for (spinner in spinners) {
+            val tickets = spinner.key.value.toInt()
+            ticketAmount += tickets
+            totalPrice += tickets * spinner.value.priceInCents
+
+        }
+        val totalView = document.getElementById("detail-total-view")!!
+        totalView.clear()
+        totalView.append(
+                label("Total", "cat-total-price-label"),
+                span("${totalPrice / 100.0}€", "cat-total-price"),
+                label("Amount", "cat-total-amount-label"),
+                span("$ticketAmount", "cat-total-amount")
+        )
+    }
+
+    private fun detailViewInfo(event: EventDTO, occurrence: Occurrence): Element {
+        return document.createElement("div") {
+            //TODO
+        }
     }
 
     private fun td(child: Element): Element {
@@ -130,21 +236,266 @@ object DomModifier {
     }
 
     fun Occurrence.toLocationString() = "$country, $city"
+    fun PriceCategory.toDisplayPrice() = "${priceInCents / 100.0}€"
+    fun PriceCategory.ticketRange() = "$usedTickets / $totalTickets"
     fun Date.toDateString() = "$year-$month-$dayOfMonth"
     fun Time.toTimeString() = "$hour:$minute"
+
+    fun updateCart() {
+        val cartContainer = document.getElementById("cart")!!
+        cartContainer.clear()
+        val asList = Cart.asList()
+        if (asList.isNotEmpty()) {
+            for (item in asList) {
+                if (item.isNotEmpty()) {
+                    val first = item.first()
+                    cartContainer.appendElement("div") {
+                        className = "cart-row"
+                        append(
+                                span(first.event.title, "cart-evt-title"),
+                                span(first.occurrence.date.toDateString() + ":" + first.occurrence.time.toTimeString(), "cart-date-time"),
+                                span(first.category.categoryName, "cart-cat-name"),
+                                span((first.category.priceInCents / 100.0).toString(), "cart-cat-price"),
+                                span(item.size.toString(), "cart-cat-amount-tickets")
+                        )
+                    }
+                }
+            }
+        } else {
+            cartContainer.appendElement("h3") {
+                this.textContent = "Your Cart is Empty"
+            }
+
+        }
+    }
+
+    fun updateBrowser(): Promise<Boolean> {
+        return getSearchResults(latestSearchQuery).then {
+            populateContainer(it)
+            return@then it.isNotEmpty()
+        }.catch {
+            handleError("Could not query search results")
+            return@catch false
+        }
+    }
+
+    private fun handleError(s: String) {
+        OnPageAlert.showErr(s)
+    }
+}
+
+fun openCart() {
+    Handler.updateCart()
+    openTab("cart")
+}
+
+fun openBrowser() {
+    Handler.updateBrowser().then {
+        if (it) {
+            openTab("container")
+        }
+    }
+}
+
+fun openTab(id: String) {
+    val elementsByClassName = document.getElementsByClassName("tabcontent")
+    for (i in 0 until elementsByClassName.length) {
+        val element = elementsByClassName[i]
+        element!!.addClass("none")
+        if (element.id == id) {
+            element.removeClass("none")
+        }
+    }
 }
 
 fun main(args: Array<String>) {
-    val searchButton = document.getElementById("searchBtn") as HTMLButtonElement
-    searchButton.addEventListener(type = "click", callback = {
-        val handler = Handler()
-        val searchQuery = "-title=\"spon\""
-        val searchQueryEmpty = ""
-        val searchResults = handler.getSearchResults(searchQueryEmpty)
-        searchResults.then {
-            console.dir(it)
-            DomModifier.populateContainer(it)
+    /* Notification code if needed
+    //Request Permissions
+    if (Notification.permission != NotificationPermission.GRANTED) {
+        console.log("Permission Missing")
+        Notification.requestPermission().then {
+            if (it == NotificationPermission.GRANTED) {
+                console.info("Notifications active")
+                Notification("Activated Notifications")
+            } else {
+                console.info("Not granted")
+            }
         }
+    } else {
+        console.info("Notifications active")
+        Notification("Active Notifications")
+    }
+     */
+    //Setup tab buttons
+    (document.getElementById("searchTabBtn") as HTMLButtonElement)
+            .addEventListener(type = "click", callback = {
+                openTab("search")
+            })
+    (document.getElementById("containerBtn") as HTMLButtonElement)
+            .addEventListener(type = "click", callback = {
+                openBrowser()
+            })
+    (document.getElementById("cartBtn") as HTMLButtonElement)
+            .addEventListener(type = "click", callback = {
+                openCart()
+            })
+    //Open default tab
+    openTab("search")
+    (document.getElementById("searchBtn") as HTMLButtonElement)
+            .addEventListener(type = "click", callback = {
+                generateSearchQuery()
+                openBrowser()
+            })
+    val resetSearchButton = document.getElementById("resetBtn") as HTMLButtonElement
+    resetSearchButton.addEventListener(type = "click", callback = {
+        val searchEvtTitle = document.getElementById("event-title") as HTMLInputElement
+        val withFromCB = document.getElementById("withFrom") as HTMLInputElement
+        val fromDatePick = document.getElementById("fromDate") as HTMLInputElement
+        val withTillCB = document.getElementById("withTill") as HTMLInputElement
+        val tillDatePicker = document.getElementById("tillDate") as HTMLInputElement
+        val genreIn = document.getElementById("genre") as HTMLInputElement
+        val artistIn = document.getElementById("artist") as HTMLInputElement
+        val locationIn = document.getElementById("location") as HTMLInputElement
+
+        searchEvtTitle.value = ""
+        withFromCB.checked = false
+        withTillCB.checked = false
+        fromDatePick.value = ""
+        tillDatePicker.value = ""
+        genreIn.value = ""
+        artistIn.value = ""
+        locationIn.value = ""
     })
 }
 
+fun String.append(s: String): String {
+    return this.plus(s)
+}
+
+fun generateSearchQuery() {
+    val searchEvtTitle = document.getElementById("event-title") as HTMLInputElement
+    val withFromCB = document.getElementById("withFrom") as HTMLInputElement
+    val fromDatePick = document.getElementById("fromDate") as HTMLInputElement
+    val withTillCB = document.getElementById("withTill") as HTMLInputElement
+    val tillDatePicker = document.getElementById("tillDate") as HTMLInputElement
+    val genreIn = document.getElementById("genre") as HTMLInputElement
+    val artistIn = document.getElementById("artist") as HTMLInputElement
+    val locationIn = document.getElementById("location") as HTMLInputElement
+
+    var sb = ""
+    if (withFromCB.checked) {
+        sb = sb.append("-").append(SearchCategories.DATE_FROM.getIdf()).append("=\"").append(fromDatePick.value).append("\"")
+    }
+    if (withTillCB.checked) {
+        sb = sb.append("-").append(SearchCategories.DATE_UNTIL.getIdf()).append("=\"").append(tillDatePicker.value).append("\"")
+    }
+    if (searchEvtTitle.value.isNotEmpty()) {
+        sb = sb.append("-").append(SearchCategories.EVENT_NAME.getIdf()).append("=\"").append(searchEvtTitle.value).append("\"")
+    }
+    if (artistIn.value.isNotEmpty()) {
+        sb = sb.append("-").append(SearchCategories.ARTIST_NAME.getIdf()).append("=\"").append(artistIn.value).append("\"")
+    }
+    if (genreIn.value.isNotEmpty()) {
+        sb = sb.append("-").append(SearchCategories.GENRE.getIdf()).append("=\"").append(genreIn.value).append("\"")
+    }
+    if (locationIn.value.isNotEmpty()) {
+        sb = sb.append("-").append(SearchCategories.LOCATION.getIdf()).append("=\"").append(locationIn.value).append("\"")
+    }
+    Handler.latestSearchQuery = sb
+}
+
+enum class SearchCategories(private val catIdf: String) {
+    DATE_FROM("from"), DATE_UNTIL("until"),
+    EVENT_NAME("title"), ARTIST_NAME("name"),
+    GENRE("genre"), LOCATION("location");
+
+    fun getIdf(): String {
+        return catIdf
+    }
+}
+
+object Cart {
+    val items: MutableMap<Int, MutableList<LocalTicket>> = mutableMapOf()
+
+    fun asList(): List<List<LocalTicket>> {
+        val outerList: MutableList<List<LocalTicket>> = mutableListOf()
+        for (item in items) {
+            outerList.add(item.value)
+        }
+        return outerList
+    }
+
+    fun add(localTicket: LocalTicket) {
+        if (items.contains(localTicket.category.eventCategoryId)) {
+            items[localTicket.category.eventCategoryId]?.add(localTicket)
+        } else {
+            items[localTicket.category.eventCategoryId] = mutableListOf(localTicket)
+        }
+    }
+}
+
+
+data class LocalTicket(
+        val event: EventDTO,
+        val occurrence: Occurrence,
+        val category: PriceCategory
+)
+
+data class TicketPayload(
+        val amount: Int,
+        val bookingInfo: BookingInfo,
+        val rowseats: List<Any>
+)
+
+data class BookingInfo(
+        val cardNr: Int,
+        val city: String,
+        val country: String,
+        val house: String,
+        val nameF: String,
+        val nameL: String,
+        val street: String,
+        val zip: String
+)
+
+object OnPageAlert {
+    fun showInfo(text: String) {
+        val element = document.getElementById("alertBox")!!
+        element.textContent = text
+        element.removeClass("alertErr", "alertSuc", "none")
+        element.addClass("alertInfo")
+        addCloseBtn(element)
+        window.setTimeout(handler = {
+            element.addClass("none")
+        }, timeout = 3000)
+    }
+
+    fun showErr(text: String) {
+        val element = document.getElementById("alertBox")!!
+        element.textContent = text
+        element.removeClass("alertInfo", "alertSuc", "none")
+        element.addClass("alertErr")
+        addCloseBtn(element)
+    }
+
+    fun showSuc(text: String) {
+        val element = document.getElementById("alertBox")!!
+        element.textContent = text
+        element.removeClass("alertInfo", "alertErr", "none")
+        element.addClass("alertSuc")
+        addCloseBtn(element)
+        window.setTimeout(handler = {
+            element.addClass("none")
+        }, timeout = 3000)
+    }
+
+    private fun addCloseBtn(element: Element) {
+        element.appendElement("span") {
+            addClass("closebtn")
+            addEventListener(type = "click", callback = {
+                element.addClass("none")
+            })
+            textContent = "×"
+        }
+    }
+}
