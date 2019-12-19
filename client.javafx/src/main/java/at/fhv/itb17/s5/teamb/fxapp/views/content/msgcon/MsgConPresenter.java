c0b9e8dceaa8556ff759e0858b667e239e34b595
Matthias Rupp <mru3383@students.fhv.at>
2019-12-19 16:09:04 +0100

    Toggle Buttons for topics now exist (impl still missing), button for acknowledging all messages at once exists and works

--- client.javafx/src/main/java/at/fhv/itb17/s5/teamb/fxapp/views/content/msgcon/MsgConPresenter.java
+++ client.javafx/src/main/java/at/fhv/itb17/s5/teamb/fxapp/views/content/msgcon/MsgConPresenter.java
@@ -1,5 +1,6 @@
 package at.fhv.itb17.s5.teamb.fxapp.views.content.msgcon;
 
+import at.fhv.itb17.s5.teamb.dtos.MsgTopicDTO;
 import at.fhv.itb17.s5.teamb.fxapp.data.MsgWrapper;
 import at.fhv.itb17.s5.teamb.fxapp.viewmodel.MsgTopicVM;
 import at.fhv.itb17.s5.teamb.fxapp.viewnavigation.ContentfulViewLifeCycle;
@@ -8,9 +9,13 @@ import at.fhv.itb17.s5.teamb.fxapp.views.content.msgcon.msg.MsgPresenter;
 import at.fhv.itb17.s5.teamb.fxapp.views.content.msgcon.msg.MsgView;
 import at.fhv.itb17.s5.teamb.fxapp.views.menu.ApplicationMenuViews;
 import com.jfoenix.controls.JFXListView;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
 
 import javax.jms.JMSException;
@@ -29,6 +34,8 @@ public class MsgConPresenter implements ContentfulViewLifeCycle<MsgTopicVM> {
     private VBox msgprodVbox;
     @FXML
     private Button msgprodBtn;
+    @FXML
+    private Button ackAll;
 
     @Override
     public void onCreate(MsgTopicVM viewModel, NavigationStackActions<MsgTopicVM> navActions) {
@@ -37,6 +44,24 @@ public class MsgConPresenter implements ContentfulViewLifeCycle<MsgTopicVM> {
             rootPlane.getChildren().remove(msgprodVbox);
         } else {
             msgprodBtn.setOnAction(e -> navActions.changeToMenuItem(ApplicationMenuViews.MSG_PROD_VIEW, true));
+        }
+        ackAll.setOnAction(e -> {
+            List<MsgWrapper> allMsgs = viewModel.getAllMsgs();
+            if (!allMsgs.isEmpty()) {
+                for (MsgWrapper allMsg : allMsgs) {
+                    try {
+                        allMsg.getTextMessage().acknowledge();
+                    } catch (JMSException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
+        List<MsgTopicDTO> allTopics = viewModel.getAllTopics();
+        for (MsgTopicDTO topic : allTopics) {
+            ToggleButton toggleButton = new ToggleButton(topic.getName());
+            //TODO implement action for toggle button
+            topicFilterList.getChildren().add(toggleButton);
         }
     }
 
