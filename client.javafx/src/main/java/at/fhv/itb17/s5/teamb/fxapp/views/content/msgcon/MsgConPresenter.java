df2a2eb8d5c087181fdd5cb60f9fef92d2a4f3ff
Matthias Rupp <mru3383@students.fhv.at>
2019-12-21 15:55:55 +0100

    TopicFilterList is now fully implemented

--- client.javafx/src/main/java/at/fhv/itb17/s5/teamb/fxapp/views/content/msgcon/MsgConPresenter.java
+++ client.javafx/src/main/java/at/fhv/itb17/s5/teamb/fxapp/views/content/msgcon/MsgConPresenter.java
@@ -9,16 +9,14 @@ import at.fhv.itb17.s5.teamb.fxapp.views.content.msgcon.msg.MsgPresenter;
 import at.fhv.itb17.s5.teamb.fxapp.views.content.msgcon.msg.MsgView;
 import at.fhv.itb17.s5.teamb.fxapp.views.menu.ApplicationMenuViews;
 import com.jfoenix.controls.JFXListView;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
 
 import javax.jms.JMSException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,6 +34,7 @@ public class MsgConPresenter implements ContentfulViewLifeCycle<MsgTopicVM> {
     private Button msgprodBtn;
     @FXML
     private Button ackAll;
+    private HashMap<String, ToggleButton> toggles = new HashMap<>();
 
     @Override
     public void onCreate(MsgTopicVM viewModel, NavigationStackActions<MsgTopicVM> navActions) {
@@ -57,20 +56,20 @@ public class MsgConPresenter implements ContentfulViewLifeCycle<MsgTopicVM> {
                 }
             }
         });
-        List<MsgTopicDTO> allTopics = viewModel.getAllTopics();
+        List<MsgTopicDTO> allTopics = viewModel.getSubscribedTopics();
         for (MsgTopicDTO topic : allTopics) {
             ToggleButton toggleButton = new ToggleButton(topic.getName());
-            //TODO implement action for toggle button
+            toggleButton.setOnAction(e -> onReturned(viewModel));
             topicFilterList.getChildren().add(toggleButton);
+            toggles.put(topic.getName(), toggleButton);
         }
     }
 
     @Override
     public void onReturned(MsgTopicVM viewModel) {
-        //TODO Display the existing topics
         notificationLV.getItems().clear();
         List<MsgWrapper> msgs = viewModel.getAllMsgs();
-        List<Parent> collect = msgs.stream().map(msg -> createView(msg, () -> {
+        List<Parent> collect = msgs.stream().filter(msg -> !(toggles.get(msg.getTopicName()).isSelected())).map(msg -> createView(msg, () -> {
             try {
                 viewModel.ack(msg);
             } catch (JMSException e) {
