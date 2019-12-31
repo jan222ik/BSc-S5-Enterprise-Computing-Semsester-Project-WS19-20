246e9a640bd580ae88b58da763e6cdda368d6b3c
Matthias Rupp <mru3383@students.fhv.at>
2019-12-31 22:54:21 +0100

    Removed unnecessary logging statements for Messaging classes

--- client.javafx/src/main/java/at/fhv/itb17/s5/teamb/fxapp/data/msg/MsgAsyncServiceImpl.java
+++ client.javafx/src/main/java/at/fhv/itb17/s5/teamb/fxapp/data/msg/MsgAsyncServiceImpl.java
@@ -51,7 +51,6 @@ public class MsgAsyncServiceImpl implements ExceptionListener, MessageListener, 
         session = connection.createSession(false, ActiveMQSession.INDIVIDUAL_ACKNOWLEDGE);
         // Create the destination (Topic or Queue)
         for (MsgTopicDTO msgTopic : topics) {
-            logger.info("Consumer Topic is {}", msgTopic.getName());
             Topic topic = session.createTopic("VirtualTopic." + msgTopic.getName());
             destinations.put(msgTopic.getName(), topic);
             subNames.put(topic, msgTopic.getName());
@@ -91,15 +90,11 @@ public class MsgAsyncServiceImpl implements ExceptionListener, MessageListener, 
     public synchronized void onMessage(Message message) {
         if (message instanceof TextMessage) {
             TextMessage textMessage = (TextMessage) message;
-            logger.debug("textMessage = {}", textMessage);
             String text;
             try {
                 text = textMessage.getText();
                 String topic = textMessage.getStringProperty("topic");
                 String header = textMessage.getStringProperty("header");
-                logger.debug("{} Received Topic: {}", this.hashCode(), topic);
-                logger.debug("Received header: {}", header);
-                logger.debug("Received text: {}", text);
                 MsgWrapper msgWrapper = new MsgWrapper(topic, text, textMessage, LocalDateTime.now(), false, header);
                 Platform.runLater(() -> outList.add(msgWrapper));
             } catch (JMSException e) {
